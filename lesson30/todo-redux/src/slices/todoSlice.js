a1c862f1ffad142c48845a6be14e152939b34ce6
Valerii-17 <paskal.paint@gmail.com>
2026-06-21 19:20:13 +0200

    Homework 30.2

---
+++ lesson30/todo-redux/src/slices/todoSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    todos: [],
+};
+
+const todoSlice = createSlice({
+    name: "todo",
+    initialState,
+
+    reducers: {
+        addTodo: (state, action) => {
+            state.todos.push(action.payload);
+        },
+
+        deleteTodo: (state, action) => {
+            state.todos.splice(action.payload, 1);
+        },
+    },
+});
+
+export const { addTodo, deleteTodo } = todoSlice.actions;
+
+export default todoSlice.reducer;
